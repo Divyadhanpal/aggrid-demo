6434feb3c45ac9650024ed9deba68a3a5fbf4ea5
venky4work <venky4work@gmail.com>
2021-11-04 09:59:53 +0530

    serverside scroll scenario

--- src/App.js
+++ src/App.js
@@ -4,6 +4,7 @@ import "ag-grid-community/dist/styles/ag-theme-balham.css";
 import { AgGridReact, AgGridColumn } from "ag-grid-react";
 import "ag-grid-enterprise";
 
+
 export const App = (props) => {
   const [gridApi, setGridApi] = useState(null);
   const [gridColumnApi, setGridColumnApi] = useState(null);
@@ -21,201 +22,76 @@ export const App = (props) => {
     if (status.includes("TVPC PENDING ACK")) {
       return "TVPC. Exception Pending";
       // console log("status", status)
-      return "Tvpc Pending, Approval";
+      // return "Tvpc Pending, Approval";
     }
   };
-  const apiJson = [
-    {
-      entityCode: "GEMDBK610",
-      counterPartyGrid: "123456",
-      valueDate: "2021-07-01",
-      runningTotalUsd: 500000.0,
-      version: 0,
-      settlements: [
-        {
-          version: 0,
-          dbPk: 1,
-          ourEntityBerId: 7,
-          status: "TVPC PENDING ACK",
-          entityCode: "GBMDBK610",
-          entityName: "HSBC HSBC BANK PLC LONDON",
-          entitySettlementMethod: "SWIFT STD",
-          entityBerSsiid: "value",
-          entityBerSsiVersion: "value",
-          counterPartyGRID: "123456",
-          counterPartyBerId: "value",
-          counterPartyName: "value",
-          ackBy: "value",
-          approvedBy: "value",
-        },
-        {
-          version: 0,
-          dbPk: 2,
-          ourEntityBerId: 7,
-          entityCode: "GBMDBK610",
-          status: "TVPC PENDING ACK",
-          entityName: "HSBC, HSBC BANK PLC LONDON",
-          entitySettlementMethod: "SWIFT _STD",
-          entityBerSsiid: "value",
-          entityBerSsiVersion: "value",
-          counterPartyGRID: "123456",
-          counterPartyBerld: "value",
-          counterPartyName: "value",
-          ackBy: "value",
-          approvedBy: "value",
-        },
-      ],
-    },
-    {
-      entityCode: "GEMDBK611",
-      counterPartyGrid: "123456",
-      valueDate: "2021-07-01",
-      runningTotalUsd: 500000.0,
-      version: 1,
-      settlements: [
-        {
-          version: 1,
-          dbPk: 1,
-          ourEntityBerId: 7,
-          status: "TVPC PENDING ACK",
-          entityCode: "GBMDBK611",
-          entityName: "HSBC HSBC BANK PLC LONDON",
-          entitySettlementMethod: "SWIFT STD",
-          entityBerSsiid: "value",
-          entityBerSsiVersion: "value",
-          counterPartyGRID: "123456",
-          counterPartyBerId: "value",
-          counterPartyName: "value",
-          ackBy: "value",
-          approvedBy: "value",
-        },
-        {
-          version: 1,
-          dbPk: 2,
-          ourEntityBerId: 7,
-          entityCode: "GBMDBK611",
-          status: "TVPC PENDING ACK",
-          entityName: "HSBC, HSBC BANK PLC LONDON",
-          entitySettlementMethod: "SWIFT _STD",
-          entityBerSsiid: "value",
-          entityBerSsiVersion: "value",
-          counterPartyGRID: "123456",
-          counterPartyBerld: "value",
-          counterPartyName: "value",
-          ackBy: "value",
-          approvedBy: "value",
-        },
-      ],
-    },
-    {
-      entityCode: "GEMDBK612",
-      counterPartyGrid: "123456",
-      valueDate: "2021-07-01",
-      runningTotalUsd: 500000.0,
-      version: 1,
-      settlements: [
-        {
-          version: 1,
-          dbPk: 1,
-          ourEntityBerId: 7,
-          status: "TVPC PENDING ACK",
-          entityCode: "GBMDBK612",
-          entityName: "HSBC HSBC BANK PLC LONDON",
-          entitySettlementMethod: "SWIFT STD",
-          entityBerSsiid: "value",
-          entityBerSsiVersion: "value",
-          counterPartyGRID: "123456",
-          counterPartyBerId: "value",
-          counterPartyName: "value",
-          ackBy: "value",
-          approvedBy: "value",
-        },
-        {
-          version: 1,
-          dbPk: 2,
-          ourEntityBerId: 7,
-          entityCode: "GBMDBK612",
-          status: "TVPC PENDING ACK",
-          entityName: "HSBC, HSBC BANK PLC LONDON",
-          entitySettlementMethod: "SWIFT _STD",
-          entityBerSsiid: "value",
-          entityBerSsiVersion: "value",
-          counterPartyGRID: "123456",
-          counterPartyBerld: "value",
-          counterPartyName: "value",
-          ackBy: "value",
-          approvedBy: "value",
-        },
-      ],
-    },
-    {
-      entityCode: "GEMDBK613",
-      counterPartyGrid: "123456",
-      valueDate: "2021-07-01",
-      runningTotalUsd: 500000.0,
-      version: 1,
-      settlements: [
-        {
-          version: 1,
-          dbPk: 1,
-          ourEntityBerId: 7,
-          status: "TVPC PENDING",
-          entityCode: "GBMDBK613",
-          entityName: "HSBC HSBC BANK PLC LONDON",
-          entitySettlementMethod: "SWIFT STD",
-          entityBerSsiid: "value",
-          entityBerSsiVersion: "value",
-          counterPartyGRID: "123456",
-          counterPartyBerId: "value",
-          counterPartyName: "value",
-          ackBy: "value",
-          approvedBy: "value",
-        },
-        {
-          version: 1,
-          dbPk: 2,
-          ourEntityBerId: 7,
-          entityCode: "GBMDBK613",
-          status: "TVPC PENDING ACK",
-          entityName: "HSBC, HSBC BANK PLC LONDON",
-          entitySettlementMethod: "SWIFT _STD",
-          entityBerSsiid: "value",
-          entityBerSsiVersion: "value",
-          counterPartyGRID: "123456",
-          counterPartyBerld: "value",
-          counterPartyName: "value",
-          ackBy: "value",
-          approvedBy: "value",
-        },
-      ],
-    },
-  ];
 
-  const gridJson = [];
-  const doubleValue = apiJson.map((elem) => {
-    const {
-      entityCode,
-      counterPartyGrid,
-      valueDate,
-      runningTotalUsd,
-      version,
-      ...rest
-    } = elem;
-    rest.settlements.forEach((child) => {
-      const obj = {
-        entityCode,
-        counterPartyGrid,
-        valueDate,
-        runningTotalUsd,
-        version,
-        ...child,
-      };
-      console.log(obj, "obj");
-      gridJson.push(obj);
-    });
-    return true;
-  });
-  console.log("gridJson", gridJson);
+  // const gridJson = [];
+  // const doubleValue = apiJson.map((elem) => {
+  //   const {
+  //     entityCode,
+  //     counterPartyGrid,
+  //     valueDate,
+  //     runningTotalUsd,
+  //     version,
+  //     ...rest
+  //   } = elem;
+  //   rest.settlements.forEach((child) => {
+  //     const obj = {
+  //       entityCode,
+  //       counterPartyGrid,
+  //       valueDate,
+  //       runningTotalUsd,
+  //       version,
+  //       ...child,
+  //     };
+  //     console.log(obj, "obj");
+  //     gridJson.push(obj);apiJson
+  //   });
+  //   return true;
+  // });
+  function FakeServer(allData) {
+  
+    return {
+      getData: function (request) {
+        var results = allData;
+  
+        return {
+          success: true,
+          rows: results,
+          lastRow: getLastRowIndex(request, results),
+        };
+      },
+    };
+
+    function getLastRowIndex(request, results) {
+        if (!results || results.length === 0) {
+          return null;
+        }
+    
+        var currentLastRow = request.startRow + results.length;
+    
+        return currentLastRow <= request.endRow ? currentLastRow : -1;
+      }
+}
+  function ServerSideDatasource(server) {
+    return {
+      getRows: function (params) {
+        console.log('[Datasource] - rows requested by grid: ', params.request);
+        var response = server.getData(params.request);
+        setTimeout(function () {
+          if (response.success) {
+            params.success({
+              rowData: response.rows,
+              rowCount: response.lastRow,
+            });
+          } else {
+            params.fail();
+          }
+        }, 200);
+      },
+    };
+  }
   const gridOptions = {
     columnDefs: [
       {
@@ -223,6 +99,7 @@ export const App = (props) => {
         sortable: true,
         unSortIcon: true,
         hide: true,
+
         rowGroup: true,
         pinned: "left",
         maxWidth: 300,
@@ -343,18 +220,121 @@ export const App = (props) => {
     ],
   };
 
-  // const onGridReady = (params) => {
-  //   setGridApi(params,api);
-  //   setGridColumnApi(params,columnApi);
-  //   const updateData = (data) => {
-  //     setRowData(data);
-  //   }
-  // }
+  const onGridReady = (params) => {
+    setGridApi(params.api);
+    setGridColumnApi(params.columnApi);
+   
+    const updateData = (data) => {
+      const gridJson = [];
+      data.map((elem) => {
+        const {
+          entityCode,
+          counterPartyGrid,
+          valueDate,
+          runningTotalUsd,
+          version,
+          ...rest
+        } = elem;
+        rest.settlements.forEach((child) => {
+          const obj = {
+            entityCode,
+            counterPartyGrid,
+            valueDate,
+            runningTotalUsd,
+            version,
+            ...child,
+          };
+          console.log(obj, "obj");
+          gridJson.push(obj);
+        });
+      });
+      var fakeServer = new FakeServer(gridJson);
+      var datasource = new ServerSideDatasource(fakeServer);
+      params.api.setServerSideDatasource(datasource);
+    };
+
+    fetch(`data.json`, {
+      headers: {
+        "Content-Type": "application/json",
+        Accept: "application/json",
+      },
+    })
+      .then((resp) => resp.json())
+      .then((data) => updateData(data));
+  };
+
+  const sortAndFilter = (allOfTheData, sortModel, filterModel) => {
+    return sortData(sortModel, filterData(filterModel, allOfTheData));
+  };
+  const sortData = (sortModel, data) => {
+    var sortPresent = sortModel && sortModel.length > 0;
+    if (!sortPresent) {
+      return data;
+    }
+    var resultOfSort = data.slice();
+    resultOfSort.sort(function (a, b) {
+      for (var k = 0; k < sortModel.length; k++) {
+        var sortColModel = sortModel[k];
+        var valueA = a[sortColModel.colId];
+        var valueB = b[sortColModel.colId];
+        if (valueA == valueB) {
+          continue;
+        }
+        var sortDirection = sortColModel.sort === "asc" ? 1 : -1;
+        if (valueA > valueB) {
+          return sortDirection;
+        } else {
+          return sortDirection * -1;
+        }
+      }
+      return 0;
+    });
+    return resultOfSort;
+  };
+  const filterData = (filterModel, data) => {
+    var filterPresent = filterModel && Object.keys(filterModel).length > 0;
+    if (!filterPresent) {
+      return data;
+    }
+    var resultOfFilter = [];
+    for (var i = 0; i < data.length; i++) {
+      var item = data[i];
+      if (filterModel.age) {
+        var age = item.age;
+        var allowedAge = parseInt(filterModel.age.filter);
+        if (filterModel.age.type == "equals") {
+          if (age !== allowedAge) {
+            continue;
+          }
+        } else if (filterModel.age.type == "lessThan") {
+          if (age >= allowedAge) {
+            continue;
+          }
+        } else {
+          if (age <= allowedAge) {
+            continue;
+          }
+        }
+      }
+      if (filterModel.year) {
+        if (filterModel.year.values.indexOf(item.year.toString()) < 0) {
+          continue;
+        }
+      }
+      if (filterModel.country) {
+        if (filterModel.country.values.indexOf(item.country) < 0) {
+          continue;
+        }
+      }
+      resultOfFilter.push(item);
+    }
+    return resultOfFilter;
+  };
 
   return (
     <div
       className="ag-theme-balham"
-      style={{ height: "650px", width: "1200px" }}
+      style={{ height: "300px", width: "1200px" }}
     >
       <AgGridReact
         className=""
@@ -372,33 +352,62 @@ export const App = (props) => {
           lockPinned: true,
           minhidth: 250,
           cellRenderer: "agGroupCellRenderer",
-          cellRendererParams: { checkbox:  function(params) {
-            const isGroupRow = params.node.level === 0; //only show the checkbox on group row. 
-            console.log(isGroupRow);
-            if(isGroupRow) {
-              const childRows = params.node.allLeafChildren;
-              if(childRows) {
-                console.log(childRows.filter(e => e.data.status === 'TVPC PENDING ACK').length);
-              }
-              if (childRows && childRows.filter(e => e.data.status === 'TVPC PENDING ACK').length > 0) {
-                return false;
+          cellRendererParams: {
+            checkbox: function (params) {
+              const isGroupRow = params.node.level === 0; //only show the checkbox on group row.
+              console.log(isGroupRow);
+              if (isGroupRow) {
+                const childRows = params.node.allLeafChildren;
+                if (childRows) {
+                  console.log(
+                    childRows.filter(
+                      (e) => e.data.status === "TVPC PENDING ACK"
+                    ).length
+                  );
+                }
+                if (
+                  childRows &&
+                  childRows.filter((e) => e.data.status === "TVPC PENDING ACK")
+                    .length > 0
+                ) {
+                  return false;
+                } else {
+                  return true;
+                }
               } else {
                 return true;
               }
-            } else {
-                return true;
-            }
-        } },
+            },
+          },
         }}
         rowSelection="multiple"
         groupSelectsChildren
         suppressRowClickSelection
         suppressAggFuncInHeader
+        // pagination
         // suppressAggFilteredOnly
-        //onGridReady={onGridReady}
-        rowData={gridJson}
+        onGridReady={onGridReady}
+        // rowData={gridJson}
         onRowDoubleClicked={(item) => selectRow(item)}
         groupSelectsFiltered
+        components={{
+          loadingRenderer: function (params) {
+            if (params.value !== undefined) {
+              return params.value;
+            } else {
+              return '<img src="https://www.ag-grid.com/example-assets/loading.gif">';
+            }
+          },
+        }}
+        rowBuffer={0}
+        rowModelType={"serverSide"}
+        serverSideStoreType={'partial'}
+        // paginationPageSize={2}
+        // cacheOverflowSize={2}
+        // maxConcurrentDatasourceRequests={2}
+        // infiniteInitialRowCount={1}
+        // maxBlocksInCache={2}
+        cellRenderer="loadingRenderer"
       />
     </div>
   );
